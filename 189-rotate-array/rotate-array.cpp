ff97e9551f77eacd70318c0ec02c45b4239ca3f0
Priyanshu Gupta <priyanshug.cs.25@nitj.ac.in>
2026-04-02 23:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (66.55%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        // Doing in n(1) Space complexity
         int n = nums.size();
-        vector<int> temp(n);
+        k = k % n;
 
-        for(int i=0; i<nums.size(); i++){
-            temp[(i+k) % n] = nums[i]; 
-        }
-        nums = temp;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
