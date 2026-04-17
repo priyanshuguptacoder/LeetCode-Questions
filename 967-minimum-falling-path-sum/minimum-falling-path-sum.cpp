683f7f01adab80b8742f3501613b33c002928b1c
Priyanshu Gupta <priyanshug.cs.25@nitj.ac.in>
2026-04-17 23:10:34 +0530

    Time: 4 ms (21.61%) | Memory: 14.9 MB (75.60%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -3,19 +3,21 @@ public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> prev(n);
 
         for(int j=0; j<n; j++){
-            dp[0][j] = matrix[0][j]; //First row as base case
+            prev[j] = matrix[0][j]; //First row as base case
         }
 
         for(int i=1; i<m; i++){
+            vector<int> curr(n);
+
             for(int j=0; j<n; j++){
-                int up = matrix[i][j] + dp[i-1][j];
+                int up = matrix[i][j] + prev[j];
 
                 int ld = matrix[i][j];
                 if(j > 0){
-                    ld += dp[i-1][j-1];
+                    ld += prev[j-1];
                 }
                 else{
                     ld += 1e9;
@@ -23,19 +25,20 @@ public:
 
                 int rd = matrix[i][j];
                 if(j < n-1){
-                    rd += dp[i-1][j+1];
+                    rd += prev[j+1];
                 }
                 else{
                     rd += 1e9;
                 }
 
-                dp[i][j] = min(up, min(ld, rd));
+                curr[j] = min(up, min(ld, rd));
             }
+            prev = curr;
         }
 
         int mini = 1e9;
         for(int j=0; j<n; j++){
-            mini = min(mini, dp[n-1][j]); //Last Rom me maximum kon hai wo answer hoga
+            mini = min(mini, prev[j]); //Last Rom me maximum kon hai wo answer hoga
         }
         return mini;
     }
